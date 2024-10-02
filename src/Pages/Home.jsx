8d4df328b4e155fc2b53a80b6e7bde39e5d264c4
Amysly <amarachukwuobiegue@gmail.com>
2024-10-02 19:35:16 +0100

    portfolio commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,38 @@
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaLinkedin} from "react-icons/fa";
+import { FaTwitterSquare} from "react-icons/fa";
+const Home = () => {
+    return ( 
+        <div className="h-full w-full bg-cover bg-center">
+        <div className="pt-4 px-4 background">
+        <div className="text-white p-4">
+          <div className=" font-mono text-center mt-32">
+          <div className="profile-div mb-8">
+            <img  src="/images/amara.jpg" className="profile-pic mt-8"/>
+            </div>
+            <div className="font-serif">
+              <h1 className="text-3xl lg:text-5xl  mb-8 text-yellow-400 font-serif ">Amarachi Obiegue</h1>
+              <p className="sm:text-2xl lg:text-3xl">I'm a passionate fontend developer</p>
+              <p className="sm:text-2xl lg:text-3xl"> I provide clean code and pixel perfect  design.</p>
+              <p className="sm:text-2xl lg:text-3xl">I also make website more & more interactive with web animations.</p>
+            </div>
+           
+          <div className="flex justify-center space-x-4 mt-10 mb-10 sm:text-2xl lg:text-5xl">
+            <FaTwitterSquare className="bg-zinc-700 p-2 rounded-full hover:bg-yellow-400"/>
+            <FaFacebook className="bg-zinc-700 p-2 rounded-full hover:bg-yellow-400"/>
+            <FaInstagramSquare className="bg-zinc-700 p-2 rounded-full hover:bg-yellow-400"/>
+            <FaLinkedin className="bg-zinc-700 p-2 rounded-full hover:bg-yellow-400"/>
+        </div>
+          </div>
+          
+        </div>
+     <div>
+    
+     </div>
+     </div>
+    </div>
+     );
+  }
+  
+  export default Home;
---
+++ src/Pages/Project.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+
+
+const myProjects = [
+    {
+        id: 1,
+        title: "Address-Book App",
+        image: "/images/address.jpg", 
+        description: "Created with Html,Css and Javascript",
+        liveLink: "https://warm-zuccutto-d0acb8.netlify.app",
+        codeLink: "https://github.com/Amysly/AddressBook"
+    },
+
+    {
+        id: 2,
+        title: "To-do-list App",
+        description: "Created with Html,Css and Javascript",
+        image: "/images/todolist.jpg", 
+        liveLink: "https://preeminent-longma-18f0f3.netlify.app",
+        codeLink: "https://github.com/Amysly/Todolist"
+    },
+
+    {
+        id: 3,
+        image: "/images/cart.jpg", 
+        title: "E-commerce website",
+        description: "Created with Html,Css and Javascript",
+        liveLink: "https://jovial-cactus-77d66f.netlify.app/",
+        codeLink: "https://github.com/Amysly/Amy-Kitchen"
+    },
+]
+const Project = () => {
+  return (
+    <div className="background-two">
+    <div className="pt-4 px-4 background">
+    <h2 className="col-span-full text-yellow-400 text-3xl lg:text-4xl text-center font-serif 
+          lg:mt-20 "> Projects</h2>
+          <div className="border border-yellow-400 w-32 mx-auto mt-4"></div>
+        <div className='grid lg:grid-cols-2 gap-7 p-10'>
+        {myProjects.map((myProject) => (
+            <div key={myProject.id} data-aos="fade-up">
+            <img src={myProject.image} alt={myProject.title} style={{ height: "350px" }} />
+            <h2 className='text-white lg:text-2xl font-serif mt-3'>{myProject.title}</h2>
+            <h2 className='text-white lg:text-2xl font-serif mb-3'>{myProject.description}</h2>
+            
+            <a href={myProject.liveLink} target="_blank" rel="noopener noreferrer">
+                <button className='bg-yellow-400 p-1 mr-3 text-white rounded-md font-serif lg:text-2xl'>
+                Live View
+                </button>
+            </a>
+            
+            <a href={myProject.codeLink} target="_blank" rel="noopener noreferrer">
+                <button className='bg-yellow-400 p-1 text-white rounded-md font-serif lg:text-2xl'>
+                View Code
+                </button>
+            </a>
+            </div>
+        ))}
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Project
